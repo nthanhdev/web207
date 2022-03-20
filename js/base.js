e0c83708c8ed790bbf2413dc77752229cb55554c
Bemm <Bemm@gmail.com>
2022-03-20 22:30:20 +0700

    chinh sua page thanh SPA with  ng-view

--- js/base.js
+++ js/base.js
@@ -1,7 +1,34 @@
 
 
-var myApp = angular.module('app', []) ; 
+var myApp = angular.module('app', ["ngRoute"]) ; 
 
+
+myApp.config(function($routeProvider , $locationProvider){
+   $locationProvider.hashPrefix(''); // add configuration
+    $routeProvider
+        .when("/home", {
+            templateUrl: "listsubjects.html"
+        })
+        .when("/signin", {
+            templateUrl: "login.html"
+        })
+        .when("/signup", {
+            templateUrl: "reg.html"
+        })
+        .when("/contact", {
+            templateUrl: "contact.html"
+        })
+        .when("/subject/:idsubject", {
+            templateUrl: "thi.html"
+        })
+        .when("/summary/:idsummary", {
+            templateUrl: "ketqua.html"
+        })
+        .when("/history/:idhistory", {
+            templateUrl: "history.html"
+        })
+        .otherwise({templateUrl: "listsubjects.html"})
+})
 
 myApp.controller("mainController" , function($scope , $rootScope) { 
 
