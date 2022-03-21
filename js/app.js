5365e78ed778cc373b9bb25388585a47f3f270f3
Bemm <Bemm@gmail.com>
2022-03-21 20:49:14 +0700

    commit 2 chuc nang login :v

--- js/app.js
+++ js/app.js
@@ -29,39 +29,65 @@ myApp.config(function($routeProvider , $locationProvider){
         .when("/history/:idhistory", {
             templateUrl: "history.html"
         })
+        .when("/profile", {
+            templateUrl: "profile.html"
+        })
         .otherwise({templateUrl: "listsubjects.html"})
 })
 
 myApp.controller("mainController" , function($scope , $rootScope) { 
-    $rootScope.isLogin = false;
+    var userjson = sessionStorage.getItem("user");
+    if(userjson != null){
+        $rootScope.user = JSON.parse(userjson);
+        $rootScope.isLogin = true;
+    }else 
+        $rootScope.isLogin = false;
 
+    
+        $scope.logout = function() { 
+            sessionStorage.clear();
+            $rootScope.isLogin = false;
+
+            location.href = '#signin'
+            
+        }
 });
 
 
 
 myApp.controller("accountController" , function($scope , $rootScope , $http) { 
 
+    if($rootScope.isLogin){
+        location.href = '#home'
+    }
+
+    
     $scope.userLogin = {"user" : "", pass: "", isSuccess: true};
     $scope.login = function() { 
-        var data = {username:  $scope.userLogin.user ,  password: $scope.userLogin.pass  };
-        $http.post("https://localhost:5001/api/Student/Login" ,data).then(function(response){
-
-                var user = response.data;
-                if(user.success) { 
-                    $scope.userLogin.isSuccess = true;
-                    $rootScope.isLogin = true;
-                    $rootScope.user = user.infoUser;
-                    location.href = '#home'
-                }else { 
-                    $scope.userLogin.isSuccess = false; 
-                    $rootScope.isLogin = false;
-
-
-                }
-
-        })
+        
+        // check đã login chưa 
+        if(!$rootScope.isLogin){
+            var data = {username:  $scope.userLogin.user ,  password: $scope.userLogin.pass  };
+            $http.post("https://localhost:5001/api/Student/Login" ,data).then(function(response){
+    
+                    var user = response.data;
+                    if(user.success) { 
+                        $scope.userLogin.isSuccess = true;
+                        $rootScope.isLogin = true;
+                         $rootScope.user = user.infoUser;
+                        
+                        location.href = '#home'
+                        sessionStorage.setItem("user" , JSON.stringify(user.infoUser));
+                    }else { 
+                        $scope.userLogin.isSuccess = false; 
+                        $rootScope.isLogin = false;
+                    }
+    
+            })
+        }else location.href = '#home'
      
     }
+
 
 });
     
